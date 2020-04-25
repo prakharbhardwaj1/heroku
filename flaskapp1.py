bf8e77cbf560f1af084bcbcdde89592af4cb66c1
prakharbhardwaj1 <46398870+prakharbhardwaj1@users.noreply.github.com>
2020-04-26 03:54:03 +0530

    Update flaskapp1.py

--- flaskapp1.py
+++ flaskapp1.py
@@ -9,7 +9,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__ ,template_folder='template')
+app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
